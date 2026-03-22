47d85a2638223a6ac45dda3613c152d5f8890c90
ndu <OkomaNdu>
2026-03-22 00:42:42 -0400

    Update MySQL dependencies

--- mysql.tf
+++ mysql.tf
@@ -14,14 +14,14 @@ provider "kubernetes" {
 # load_config_file       = "false"
   host                   = data.aws_eks_cluster.cluster.endpoint
   token                  = data.aws_eks_cluster_auth.cluster.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
 }
 
 provider "helm" {
-  kubernetes {
-    host = data.aws_eks_cluster.cluster.endpoint
-    token = data.aws_eks_cluster_auth.cluster.token
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  kubernetes = {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   }
 }
 
